e6e98e607f8ef8ab1b950033f71b358f54c42844
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2025-10-18 21:01:08 +0200

    adjusted healtcheck

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -3,6 +3,8 @@ package app.routes;
 import io.javalin.apibuilder.EndpointGroup;
 import jakarta.persistence.EntityManagerFactory;
 
+import java.util.Map;
+
 import static io.javalin.apibuilder.ApiBuilder.get;
 
 public class Routes {
@@ -18,7 +20,7 @@ public class Routes {
     public EndpointGroup getRoutes(){
         return () -> {
             get("/", ctx -> ctx.result("Hello World!"));
-            get("healthcheck", ctx -> ctx.status(200).json("{\"msg\": \"API is up and running\"}"));
+            get("healthcheck", ctx -> ctx.status(200).json(Map.of("msg", "API is up and running")));
         } ;
     }
 }
