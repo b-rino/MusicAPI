36128ba3ba009bb8008461171b822d2b3896c39b
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2025-10-18 20:23:16 +0200

    changed the name of the log files, and added Main to test CI/CD

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
+import app.config.ApplicationConfig;
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManagerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+
+        ApplicationConfig.startServer(7074, emf);
     }
 }
